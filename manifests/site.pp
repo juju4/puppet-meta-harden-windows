bc008f7757f36cd967d27d6721d6548da7e46b9f
juju4 <juju4@users.noreply.github.com>
2018-05-05 12:54:17 -0400

    Revert "enable TLS 1.2, disable SSL 2.0 and 3.0 - remove value quotes as not string"
    This reverts commit 5efc26614a37025b13cea6250778a12f9deaa50d.

--- manifests/site.pp
+++ manifests/site.pp
@@ -468,25 +468,25 @@ node default {
       dsc_ensure => 'Present',
       dsc_key => "HKLM:\\SYSTEM\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${proto}\\Client",
       dsc_valuename => 'DisabledByDefault',
-      dsc_valuedata => 0,
+      dsc_valuedata => '0',
     }
     dsc_registry {"Client-${proto}-Enabled":
       dsc_ensure => 'Present',
       dsc_key => "HKLM:\\SYSTEM\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${proto}\\Client",
       dsc_valuename => 'Enabled',
-      dsc_valuedata => 1,
+      dsc_valuedata => '1',
     }
     dsc_registry {"Server-${proto}-DisabledByDefault":
       dsc_ensure => 'Present',
       dsc_key => "HKLM:\\SYSTEM\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${proto}\\Server",
       dsc_valuename => 'DisabledByDefault',
-      dsc_valuedata => 0,
+      dsc_valuedata => '0',
     }
     dsc_registry {"Server-${proto}-Enabled":
       dsc_ensure => 'Present',
       dsc_key => "HKLM:\\SYSTEM\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${proto}\\Server",
       dsc_valuename => 'Enabled',
-      dsc_valuedata => 1,
+      dsc_valuedata => '1',
     }
   }
   $proto_disable.each |String $proto| {
@@ -494,25 +494,25 @@ node default {
       dsc_ensure => 'Present',
       dsc_key => "HKLM:\\SYSTEM\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${proto}\\Client",
       dsc_valuename => 'DisabledByDefault',
-      dsc_valuedata => 0,
+      dsc_valuedata => '0',
     }
     dsc_registry {"Client-${proto}-Enabled":
       dsc_ensure => 'Present',
       dsc_key => "HKLM:\\SYSTEM\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${proto}\\Client",
       dsc_valuename => 'Enabled',
-      dsc_valuedata => 0,
+      dsc_valuedata => '0',
     }
     dsc_registry {"Server-${proto}-DisabledByDefault":
       dsc_ensure => 'Present',
       dsc_key => "HKLM:\\SYSTEM\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${proto}\\Server",
       dsc_valuename => 'DisabledByDefault',
-      dsc_valuedata => 0,
+      dsc_valuedata => '0',
     }
     dsc_registry {"Server-${proto}-Enabled":
       dsc_ensure => 'Present',
       dsc_key => "HKLM:\\SYSTEM\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${proto}\\Server",
       dsc_valuename => 'Enabled',
-      dsc_valuedata => 0,
+      dsc_valuedata => '0',
     }
   }
 
